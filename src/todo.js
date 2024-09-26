b7f76266a4ab35ea460d4966dad1ee2ad1782ce6
KrisztinaBrczan <brczan.kriszti@gmail.com>
2024-09-26 20:13:16 +0200

    create findByTitle function

--- src/todo.js
+++ src/todo.js
@@ -1,13 +1,13 @@
 export function format(todo) {
-  return `${todo.id} - [${todo.done ? '✅': ' '}] ${todo.title}`;
+  return `${todo.id} - [${todo.done ? "✅" : " "}] ${todo.title}`;
 }
 
 export function formatList(todos) {
-  return todos.map(format)
+  return todos.map(format);
 }
 
 function nextId(todos) {
-  const ids = todos.map(todo => todo.id);
+  const ids = todos.map((todo) => todo.id);
   if (ids.length === 0) {
     return 1;
   }
@@ -16,18 +16,30 @@ function nextId(todos) {
 }
 
 export function list(store) {
-  return store.get(); 
+  return store.get();
 }
 
 export function add(store, params) {
   const [title] = params;
-  const todos = store.get()
+  const todos = store.get();
   const newTodo = {
     title,
     done: false,
-    id: nextId(todos)
-  }
-  const toStore = [...todos, newTodo]
-  store.set(toStore)
+    id: nextId(todos),
+  };
+  const toStore = [...todos, newTodo];
+  store.set(toStore);
   return newTodo;
+}
+
+export function findByTitle(store, title) {
+  const todos = store.get();
+  // if (title.length < 3) {
+  //   throw new AppError("The title must be at least 3 characters long.");
+  // }
+  const foundTodos = todos.filter(
+    (todo) => todo.title.toLowerCase() === title.toLowerCase()
+  );
+
+  return foundTodos;
 }
